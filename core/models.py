299a3cb04e7c1bc6f872f839fb76f660eb5bc301
Elmar Aliyev <elmaraliyevdev@gmail.com>
2023-05-02 19:58:45 +0400

    Wallet fields update

--- core/models.py
+++ core/models.py
@@ -52,7 +52,7 @@ class Wallet(models.Model):
     name = models.CharField(max_length=255, unique=True)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     is_default = models.BooleanField(default=False)
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
